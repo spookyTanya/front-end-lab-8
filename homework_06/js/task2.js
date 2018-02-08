801304946a91efec6ff34644d425a8aa285f2b5b
Tanya Bilanyuk <34652839+spookyTanya@users.noreply.github.com>
2018-02-08 17:50:54 +0200

    Update task2.js

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -2,8 +2,8 @@ var usd, euro, grn1, grn2, k1, k2, k3, grn11, grn22;
 k1 = 33.4602;
 k2 = 27.1196; 
 k3 = (k1/k2).toFixed(2) ;
-euro = + prompt("Input amount of EURO ");
-usd = + prompt("Input amount of USD ");
+euro = Number (prompt("Input amount of EURO "));
+usd = Number (prompt("Input amount of USD "));
 if (euro>0 && usd>0){
 	if(parseInt(euro * k1)==(euro * k1)){
 		grn1 = euro * k1 ;
