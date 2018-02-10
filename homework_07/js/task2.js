619b4e59ab881173d805ba1c63ef01a09dd2ed84
Tanya Bilanyuk <34652839+spookyTanya@users.noreply.github.com>
2018-02-10 21:54:00 +0200

    Update task2.js

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,15 +1,13 @@
-var attempts=0, guess, prize, total=0, again=true, flag=true, time=1, pr=0;
+var attempts=0, guess, prize, total=0, again=true, time=1, pr=0;
 
 	var b = confirm('Do you want to play a game?');
 	
 	if (b==false){
 		console.log('You did not become a millionaire');
 	} else{
+		attempts=0;
 		while(true){
 		var x = Math.floor(Math.random() * 5 * time);
-		console.log(x);
-		console.log('att ='+attempts);
-		console.log('prize ='+prize);
 			switch(attempts){
 					case 0:
 						prize = 10 * Math.pow(3,pr);
@@ -52,56 +50,11 @@ var attempts=0, guess, prize, total=0, again=true, flag=true, time=1, pr=0;
 				again = confirm('Do you want to play again?');
 				if(again==false){	
 					break;
+				} else{
+					attempts=0;
+					time=1; 
+					pr=0;
 				}
 			}
 		}
 }
-
-/*else{
-					//
-					attempts=0;
-					time++;
-					console.log('second');
-					console.log(attempts2);
-					var y = Math.floor(Math.random() * 10);
-					console.log(y);
-					while(cont==true){
-						switch(attempts2){
-								case 0:
-									prize1 = 30;
-									break;
-								case 1:
-									prize1 = 15;
-									break;
-								case 2: 
-									prize1 = 6;
-									break;
-							}
-						guess = parseInt(prompt('Enter a number from 0 to 10 \nAttempts left: ' + (3-attempts2)+ '\nTotal prize: '+total+'$ \nPossible prize on current attempt: '+ prize1 + '$'));
-						attempts2++;
-						if(guess==y){
-							switch(attempts2){
-								case 1:
-									prize1 = 30;
-									break;
-								case 2:
-									prize1 = 15;
-									break;
-								case 3: 
-									prize1 = 6;
-									break;
-							}
-							total+=prize1;
-							console.log('Thank you for a game. Your prize is: '+total+'$.');
-							flag=false;
-							break;
-							}
-						if(guess!=y&&attempts2==3){
-							total=0;
-							console.log('Thank you for a game. Your prize is: '+total+'$.');
-							flag=false;
-							break;
-						}
-				}
-				//
-			}*/
