6084434de5006ead35250f67d64d3e1d40814aea
Tanya Bilanyuk <34652839+spookyTanya@users.noreply.github.com>
2018-02-21 19:38:16 +0200

    Add files via upload

---
+++ homework_10/task1.js
@@ -0,0 +1,34 @@
+  //Your debounce function goes here
+function debounce(foo, delay){
+  	var time = null;
+  	return function() {
+  		var applied = () => foo.apply(this);
+  		clearTimeout(time);
+  		time = setTimeout(applied, delay);
+  	}
+
+}
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+  iterator++;
+
+  printIteratorValue();
+}
+
+function printIteratorValue() {
+  console.log('Iterator value ', iterator);
+}
+
+var increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 2'
