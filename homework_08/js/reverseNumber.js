ccfcfb75054c656145850a3de4141c84b197bab7
Tanya Bilanyuk <34652839+spookyTanya@users.noreply.github.com>
2018-03-14 23:47:33 +0200

    Delete reverseNumber.js

--- homework_08/js/reverseNumber.js
+++
@@ -1,9 +0,0 @@
-function reverseNumber(n) {
-	s = Math.sign(n);
-	m = Math.abs(n);
-    for(var r = 0; m>0; m = Math.floor(m / 10)) {
-        r *= 10;
-        r += m % 10;
-    }
-    return r*s;
-}
